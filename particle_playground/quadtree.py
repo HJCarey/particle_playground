55569bd7e52b4a25bdbe6086d243c7a4e3a270ce
Jake <hjcarey@gmail.com>
2023-11-11 12:56:13 -0500

    aider: Added the necessary "Rectangle" class to the quadtree.py file.
    # Aider chat conversation:
    
    USER: Let's modify the quadtree.py file and add the necessary "Rectangle" class that is referenced but not defined anywhere.
    
    ASSISTANT: Sure, let's add the `Rectangle` class to the `quadtree.py` file. This class will represent the boundaries of the Quadtree and its subdivisions. It will have an `x` and `y` for the center point, and `w` and `h` for the half-width and half-height. It will also have methods `contains()` and `intersects()` for checking if a point is within the rectangle and if two rectangles intersect, respectively.
    
    Here is the *SEARCH/REPLACE block* for this change:
    
    ```python
    particle_playground\quadtree.py
    <<<<<<< SEARCH
    class Quadtree:
        def __init__(self, boundary, capacity):
            self.boundary = boundary
            self.capacity = capacity
            self.atoms = []
            self.divided = False
    =======
    class Rectangle:
        def __init__(self, x, y, w, h):
            self.x = x
            self.y = y
            self.w = w
            self.h = h
    
        def contains(self, atom):
            return (atom.x >= self.x - self.w and
                    atom.x < self.x + self.w and
                    atom.y >= self.y - self.h and
                    atom.y < self.y + self.h)
    
        def intersects(self, range):
            return not (range.x - range.w > self.x + self.w or
                        range.x + range.w < self.x - self.w or
                        range.y - range.h > self.y + self.h or
                        range.y + range.h < self.y - self.h)
    
    class Quadtree:
        def __init__(self, boundary, capacity):
            self.boundary = boundary
            self.capacity = capacity
            self.atoms = []
            self.divided = False
    >>>>>>> REPLACE
    ```
    
    This *SEARCH/REPLACE block* adds the `Rectangle` class above the `Quadtree` class in the `quadtree.py` file. The `contains()` method checks if a given atom is within the rectangle, and the `intersects()` method checks if the rectangle intersects with another rectangle.

--- particle_playground/quadtree.py
+++ particle_playground/quadtree.py
@@ -1,3 +1,22 @@
+class Rectangle:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def contains(self, atom):
+        return (atom.x >= self.x - self.w and
+                atom.x < self.x + self.w and
+                atom.y >= self.y - self.h and
+                atom.y < self.y + self.h)
+
+    def intersects(self, range):
+        return not (range.x - range.w > self.x + self.w or
+                    range.x + range.w < self.x - self.w or
+                    range.y - range.h > self.y + self.h or
+                    range.y + range.h < self.y - self.h)
+
 class Quadtree:
     def __init__(self, boundary, capacity):
         self.boundary = boundary
