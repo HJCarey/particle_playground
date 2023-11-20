48576d864a091f0644289bdc34e9d6ecc4088938
Jake <hjcarey@gmail.com>
2023-11-19 23:23:25 -0500

    aider: Implemented `insert`, `query`, and `subdivide` methods in `Quadtree` class.
    # Aider chat conversation:
    
    USER: Ok, let's implement the next functions then.
    
    ASSISTANT: Sure, let's implement the `Quadtree` class methods `insert`, `query`, and `subdivide` in `quadtree.cpp`. We can use the Python code in `quadtree.py` as a reference.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    quadtree.cpp
    ```cpp
    <<<<<<< SEARCH
    bool Quadtree::insert(Atom& atom) {
        // TODO: Implement this method
        return false;
    }
    =======
    bool Quadtree::insert(Atom& atom) {
        if (!boundary.contains(atom)) {
            return false;
        }
        if (atoms.size() < capacity) {
            atoms.push_back(&atom);
            return true;
        }
        if (!divided) {
            subdivide();
        }
        return (northeast->insert(atom) ||
                northwest->insert(atom) ||
                southeast->insert(atom) ||
                southwest->insert(atom));
    }
    >>>>>>> REPLACE
    ```
    
    quadtree.cpp
    ```cpp
    <<<<<<< SEARCH
    void Quadtree::query(const Rectangle& range, std::vector<Atom*>& found) const {
        // TODO: Implement this method
    }
    =======
    void Quadtree::query(const Rectangle& range, std::vector<Atom*>& found) const {
        if (!boundary.intersects(range)) {
            return;
        }
        for (Atom* atom : atoms) {
            if (range.contains(*atom)) {
                found.push_back(atom);
            }
        }
        if (divided) {
            northeast->query(range, found);
            northwest->query(range, found);
            southeast->query(range, found);
            southwest->query(range, found);
        }
    }
    >>>>>>> REPLACE
    ```
    
    quadtree.cpp
    ```cpp
    <<<<<<< SEARCH
    void Quadtree::subdivide() {
        // TODO: Implement this method
    }
    =======
    void Quadtree::subdivide() {
        double x = boundary.x;
        double y = boundary.y;
        double w = boundary.w / 2;
        double h = boundary.h / 2;
    
        Rectangle ne(x + w, y - h, w, h);
        northeast = new Quadtree(ne, capacity);
        Rectangle nw(x - w, y - h, w, h);
        northwest = new Quadtree(nw, capacity);
        Rectangle se(x + w, y + h, w, h);
        southeast = new Quadtree(se, capacity);
        Rectangle sw(x - w, y + h, w, h);
        southwest = new Quadtree(sw, capacity);
    
        divided = true;
    }
    >>>>>>> REPLACE
    ```
    
    Please review these changes and let me know if they are correct.

--- quadtree.cpp
+++ quadtree.cpp
@@ -23,14 +23,53 @@ Quadtree::Quadtree(const Rectangle& boundary, int capacity)
       northeast(nullptr), northwest(nullptr), southeast(nullptr), southwest(nullptr) {}
 
 bool Quadtree::insert(Atom& atom) {
-    // TODO: Implement this method
-    return false;
+    if (!boundary.contains(atom)) {
+        return false;
+    }
+    if (atoms.size() < capacity) {
+        atoms.push_back(&atom);
+        return true;
+    }
+    if (!divided) {
+        subdivide();
+    }
+    return (northeast->insert(atom) ||
+            northwest->insert(atom) ||
+            southeast->insert(atom) ||
+            southwest->insert(atom));
 }
 
 void Quadtree::query(const Rectangle& range, std::vector<Atom*>& found) const {
-    // TODO: Implement this method
+    if (!boundary.intersects(range)) {
+        return;
+    }
+    for (Atom* atom : atoms) {
+        if (range.contains(*atom)) {
+            found.push_back(atom);
+        }
+    }
+    if (divided) {
+        northeast->query(range, found);
+        northwest->query(range, found);
+        southeast->query(range, found);
+        southwest->query(range, found);
+    }
 }
 
 void Quadtree::subdivide() {
-    // TODO: Implement this method
+    double x = boundary.x;
+    double y = boundary.y;
+    double w = boundary.w / 2;
+    double h = boundary.h / 2;
+
+    Rectangle ne(x + w, y - h, w, h);
+    northeast = new Quadtree(ne, capacity);
+    Rectangle nw(x - w, y - h, w, h);
+    northwest = new Quadtree(nw, capacity);
+    Rectangle se(x + w, y + h, w, h);
+    southeast = new Quadtree(se, capacity);
+    Rectangle sw(x - w, y + h, w, h);
+    southwest = new Quadtree(sw, capacity);
+
+    divided = true;
 }
