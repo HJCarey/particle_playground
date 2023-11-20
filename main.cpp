532b99caa1787a0557ad1844ffc550850c1c6e75
Jake <hjcarey@gmail.com>
2023-11-19 23:03:23 -0500

    aider: Implemented the `create()` and `rule()` functions in `main.cpp` using the Python code in `main.py` as a reference.
    # Aider chat conversation:
    
    USER: Ok, let's go into main.cpp and build out the "create()" and "rule()" functions.
    
    ASSISTANT: To implement the `create()` and `rule()` functions in `main.cpp`, we can use the Python code in `main.py` as a reference.
    
    Here are the steps:
    
    1. In `create()`, we need to create a number of `Atom` objects with random positions and the specified color, and add them to a vector. We can use the `rand()` function from the `cstdlib` library to generate random positions.
    
    2. In `rule()`, we need to create a `Quadtree` and insert all `Atom` objects from `atoms2` into it. Then, for each `Atom` in `atoms1`, we need to calculate a force based on the positions of nearby `Atom` objects, update the velocity and position of the `Atom`, and handle collisions with the window boundaries.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    main.cpp
    ```cpp
    <<<<<<< SEARCH
    std::vector<Atom*> create(int number, const std::string& color) {
        // TODO: Implement this function
        return std::vector<Atom*>();
    }
    =======
    #include <cstdlib>  // for rand()
    
    std::vector<Atom*> create(int number, const std::string& color) {
        std::vector<Atom*> atoms;
        for (int i = 0; i < number; ++i) {
            double x = static_cast<double>(rand()) / RAND_MAX * window_size;
            double y = static_cast<double>(rand()) / RAND_MAX * window_size;
            atoms.push_back(new Atom(x, y, color));
        }
        return atoms;
    }
    >>>>>>> REPLACE
    ```
    
    main.cpp
    ```cpp
    <<<<<<< SEARCH
    void rule(std::vector<Atom*>& atoms1, std::vector<Atom*>& atoms2, double g) {
        // TODO: Implement this function
    }
    =======
    void rule(std::vector<Atom*>& atoms1, std::vector<Atom*>& atoms2, double g) {
        Quadtree quadtree(Rectangle(0, 0, window_size, window_size), 4);
        for (Atom* atom : atoms2) {
            quadtree.insert(*atom);
        }
    
        for (Atom* a : atoms1) {
            std::vector<Atom*> nearby_atoms;
            quadtree.query(Rectangle(a->position[0], a->position[1], 80, 80), nearby_atoms);
    
            double force[2] = {0.0, 0.0};
            for (Atom* b : nearby_atoms) {
                double displacement[2] = {a->position[0] - b->position[0], a->position[1] - b->position[1]};
                double distance = a->distance_to(*b);
                if (distance > 0) {
                    force[0] += g * displacement[0] / distance;
                    force[1] += g * displacement[1] / distance;
                }
            }
    
            a->velocity[0] = (a->velocity[0] + force[0]) * 0.5;
            a->velocity[1] = (a->velocity[1] + force[1]) * 0.5;
            a->position[0] += a->velocity[0];
            a->position[1] += a->velocity[1];
    
            if (a->position[0] <= 0 || a->position[0] >= window_size) {
                a->velocity[0] *= -1;
            }
            if (a->position[1] <= 0 || a->position[1] >= window_size) {
                a->velocity[1] *= -1;
            }
        }
    }
    >>>>>>> REPLACE
    ```
    
    Please note that the `window_size` variable is not defined in `main.cpp`. You need to define it before using it in the `create()` and `rule()` functions.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,50 @@
 #include "atom.h"
 #include <vector>
 
+#include <cstdlib>  // for rand()
+
 std::vector<Atom*> create(int number, const std::string& color) {
-    // TODO: Implement this function
-    return std::vector<Atom*>();
+    std::vector<Atom*> atoms;
+    for (int i = 0; i < number; ++i) {
+        double x = static_cast<double>(rand()) / RAND_MAX * window_size;
+        double y = static_cast<double>(rand()) / RAND_MAX * window_size;
+        atoms.push_back(new Atom(x, y, color));
+    }
+    return atoms;
 }
 
 void rule(std::vector<Atom*>& atoms1, std::vector<Atom*>& atoms2, double g) {
-    // TODO: Implement this function
+    Quadtree quadtree(Rectangle(0, 0, window_size, window_size), 4);
+    for (Atom* atom : atoms2) {
+        quadtree.insert(*atom);
+    }
+
+    for (Atom* a : atoms1) {
+        std::vector<Atom*> nearby_atoms;
+        quadtree.query(Rectangle(a->position[0], a->position[1], 80, 80), nearby_atoms);
+
+        double force[2] = {0.0, 0.0};
+        for (Atom* b : nearby_atoms) {
+            double displacement[2] = {a->position[0] - b->position[0], a->position[1] - b->position[1]};
+            double distance = a->distance_to(*b);
+            if (distance > 0) {
+                force[0] += g * displacement[0] / distance;
+                force[1] += g * displacement[1] / distance;
+            }
+        }
+
+        a->velocity[0] = (a->velocity[0] + force[0]) * 0.5;
+        a->velocity[1] = (a->velocity[1] + force[1]) * 0.5;
+        a->position[0] += a->velocity[0];
+        a->position[1] += a->velocity[1];
+
+        if (a->position[0] <= 0 || a->position[0] >= window_size) {
+            a->velocity[0] *= -1;
+        }
+        if (a->position[1] <= 0 || a->position[1] >= window_size) {
+            a->velocity[1] *= -1;
+        }
+    }
 }
 
 int main() {
