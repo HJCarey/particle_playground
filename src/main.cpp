645db42046893441893a87370beee64ff2654267
Jake <hjcarey@gmail.com>
2023-11-24 12:55:49 -0500

    aider: Implemented logic for the `rule()` function based on the Python example, including calculating forces between atoms, updating velocities and positions, and handling boundary conditions.
    # Aider chat conversation:
    
    USER: Let's go ahead and build the logic for the function "rule()", but base it roughly off the example found in "example.py". This example is written in Python, so we'll have to convert it into a system that works with the cod we already have in C++. Also, the "window_size" variable should be passed into the "rule" function, as this will be a boundary condition for our visualization that we will implement later.
    
    ASSISTANT: To implement the logic for the `rule()` function based on the Python example, we need to modify `src\main.cpp`:
    
    1. Add velocity attributes `vx` and `vy` to the `Atom` class.
    2. Update the `create()` function to initialize `vx` and `vy` to 0.
    3. Implement the logic in the `rule()` function to calculate the force between atoms, update their velocities and positions, and handle boundary conditions.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    src\main.cpp
    ```cpp
    <<<<<<< SEARCH
    class Atom {
    public:
        int x, y;
        std::string color;
    
        Atom(int x, int y, std::string color) : x(x), y(y), color(color) {}
    =======
    class Atom {
    public:
        int x, y, vx, vy;
        std::string color;
    
        Atom(int x, int y, std::string color) : x(x), y(y), vx(0), vy(0), color(color) {}
    >>>>>>> REPLACE
    ```
    
    src\main.cpp
    ```cpp
    <<<<<<< SEARCH
    void rule(std::vector<Atom>& atoms1, std::vector<Atom>& atoms2, double g) {
        // replace with your own logic to calculate the force between atoms
        // and update their positions and velocities accordingly
    }
    =======
    void rule(std::vector<Atom>& atoms1, std::vector<Atom>& atoms2, double g, int window_size) {
        for (auto& a : atoms1) {
            int fx = 0;
            int fy = 0;
            for (const auto& b : atoms2) {
                int dx = a.x - b.x;
                int dy = a.y - b.y;
                double d = sqrt(dx*dx + dy*dy);
                if (d > 0 && d < 80) {
                    double F = g / d;
                    fx += F * dx;
                    fy += F * dy;
                }
            }
            a.vx = (a.vx + fx) * 0.5;
            a.vy = (a.vy + fy) * 0.5;
            a.x += a.vx;
            a.y += a.vy;
            if (a.x <= 0 || a.x >= window_size) {
                a.vx *= -1;
            }
            if (a.y <= 0 || a.y >= window_size) {
                a.vy *= -1;
            }
        }
    }
    >>>>>>> REPLACE
    ```
    
    src\main.cpp
    ```cpp
    <<<<<<< SEARCH
        double g = 0.1;  // replace with your own value
        rule(red, green, g);
    =======
        double g = 0.1;  // replace with your own value
        int window_size = 100;  // replace with your own value
        rule(red, green, g, window_size);
    >>>>>>> REPLACE
    ```
    
    Please note that you need to replace the logic for generating random x and y coordinates in the `create()` function and the logic for calculating the force between atoms in the `rule()` function with your own.

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@
 
 class Atom {
 public:
-    int x, y;
+    int x, y, vx, vy;
     std::string color;
 
-    Atom(int x, int y, std::string color) : x(x), y(y), color(color) {}
+    Atom(int x, int y, std::string color) : x(x), y(y), vx(0), vy(0), color(color) {}
 
     double distance_to(const Atom& other) {
         return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2));
@@ -26,9 +26,31 @@ std::vector<Atom> create(int number, std::string color) {
     return atoms;
 }
 
-void rule(std::vector<Atom>& atoms1, std::vector<Atom>& atoms2, double g) {
-    // replace with your own logic to calculate the force between atoms
-    // and update their positions and velocities accordingly
+void rule(std::vector<Atom>& atoms1, std::vector<Atom>& atoms2, double g, int window_size) {
+    for (auto& a : atoms1) {
+        int fx = 0;
+        int fy = 0;
+        for (const auto& b : atoms2) {
+            int dx = a.x - b.x;
+            int dy = a.y - b.y;
+            double d = sqrt(dx*dx + dy*dy);
+            if (d > 0 && d < 80) {
+                double F = g / d;
+                fx += F * dx;
+                fy += F * dy;
+            }
+        }
+        a.vx = (a.vx + fx) * 0.5;
+        a.vy = (a.vy + fy) * 0.5;
+        a.x += a.vx;
+        a.y += a.vy;
+        if (a.x <= 0 || a.x >= window_size) {
+            a.vx *= -1;
+        }
+        if (a.y <= 0 || a.y >= window_size) {
+            a.vy *= -1;
+        }
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -36,7 +58,8 @@ int main(int argc, char* argv[]) {
     std::vector<Atom> green = create(200, "green");
 
     double g = 0.1;  // replace with your own value
-    rule(red, green, g);
+    int window_size = 100;  // replace with your own value
+    rule(red, green, g, window_size);
 
     return 0;
 }
